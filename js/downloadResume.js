ef2242d58671aba83741e815e945e6738a5f05a8
Sampat Jadhav <jsampat76@gmail.com>
2015-10-27 12:02:23 +0530

    sampat

--- js/downloadResume.js
+++ js/downloadResume.js
@@ -2,38 +2,83 @@ document.addEventListener("deviceready", init, false);
 var store ;
 function init() {
 
-store = cordova.file.externalRootDirectory;
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestFileSystemSuccess, null); 
 
+function onRequestFileSystemSuccess(fileSystem) { 
+        var entry=fileSystem.root; 
+        entry.getDirectory("recruitment", {create: true, exclusive: false}, onGetDirectorySuccess, onGetDirectoryFail); 
+} 
 
+function onGetDirectorySuccess(dir) { 
+     // alert("Created dir "+dir.name);
+store = cordova.file.externalRootDirectory+"/recruitment/";	  
+} 
+
+function onGetDirectoryFail(error) { 
+    // alert("Error creating directory "+JSON.stringify(error)); 
+	 store = cordova.file.externalRootDirectory;
+} 
 }
 
 
-function downloadResume(url){
-
-var fileTransfer = new FileTransfer();
-var uri = encodeURI(url);
-var pathArray = uri.split( '/' );
-filename = pathArray[pathArray.length-1];
-var fileURL = store ;
-fileTransfer.download(
-    uri,
-    fileURL+"/"+ filename,
-    function(entry) {
-        console.log("download complete: " + entry.toURL());
-		navigator.notification.confirm("Downloaded Successfully",null, "Alert", "Ok");
-									
-    },
-    function(error) {
-        console.log("download error source " + error.source);
-        console.log("download error target " + error.target);
-        console.log("upload error code" + error.code);
-    },
-    false,
-    {
-        headers: {
-            "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
-        }
+function downloadResume(url,name){
+        window.requestFileSystem(
+                     LocalFileSystem.PERSISTENT, 0, 
+                     function onFileSystemSuccess(fileSystem) {
+                     fileSystem.root.getFile(
+                                 "dummy.html", {create: true, exclusive: false}, 
+                                 function gotFileEntry(fileEntry){
+                                 var sPath = fileEntry.fullPath.replace("dummy.html","");
+                                 var fileTransfer = new FileTransfer();
+                                 fileEntry.remove();
+ 
+                                 fileTransfer.download(
+                                           url,
+                                            store+name+ ".doc",
+                                           function(theFile) {
+											navigator.notification.alert(
+											'Resume Downloaded Successfully \n'+store,  // message
+											null,         // callback
+											'Success:',            // title
+											'OK'                  // buttonName
+											);
+                                          // showLink(theFile.toURI());
+                                           },
+                                           function(error) {
+                                            navigator.notification.alert(
+											JSON.stringify(error),  // message
+											null,         // callback
+											'Fail:',            // title
+											'OK'                  // buttonName
+											);
+                                           }
+                                           );
+                                 }, 
+                                 fail);
+                     }, 
+                     fail);
+ 
     }
-);
-
-}
+ 
+    function showLink(url){
+        alert(url);
+        var divEl = document.getElementById("ready");
+		 alert(JSON.stringify(evt));
+        var aElem = document.createElement("a");
+        aElem.setAttribute("target", "_blank");
+        aElem.setAttribute("href", url);
+        aElem.appendChild(document.createTextNode("Ready! Click To Open."))
+        divEl.appendChild(aElem);
+ 
+    }
+ 
+ 
+    function fail(evt) 
+	{
+		navigator.notification.alert(
+		JSON.stringify(evt),  // message
+		null,         // callback
+		'Fail:',            // title
+		'OK'                  // buttonName
+		);
+    }
